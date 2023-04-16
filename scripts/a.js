70258cc399facbfe3a14e99f47f62445326725dc
Huzaira <huzziha@gmail.com>
2023-04-16 19:06:44 +0100

    FEAT: JS ASSIGNMENT

---
+++ scripts/a.js
@@ -0,0 +1,135 @@
+// var zakatPercentage = 0.025
+// var UserInputToCalculateZakat = + prompt ("User Input To Calculate Zakat")
+// var result = zakatPercentage*UserInputToCalculateZakat
+// alert("Your Zakat value is ="+result)
+
+
+
+
+
+
+
+
+// var FamilyMembers= +prompt("Family Members");
+// var Method= prompt('Insert Fitrah Method: "Flour", "Sugar", " Pulses", "Dates" ');
+// var rate= 0;
+// var result= 0;
+
+// if(Method== "Flour"){
+//     rate=125;
+//     result= FamilyMembers*rate;
+// }
+// elseif=(Method=="Sugar")
+
+//   {  rate=300;
+//     result= FamilyMembers*rate;}
+
+// elseif=(Method=="Pulses")
+//     {rate=500;
+//     result= FamilyMembers*rate;
+// }
+// elseif=(Method=="Dates")
+//   {  rate=450;
+//     result= FamilyMembers*rate;
+// }
+
+// alert("Your Fitrah is:" +result);
+
+
+
+
+
+
+
+
+
+
+// var SecretNumber= + prompt("Guess The Secret Number")
+ 
+// if(SecretNumber== '4'){
+//     alert("Congratulations, You Guessed The Secret Number")
+
+// }
+// else if(SecretNumber <=4){
+//     alert("You Value is low, Guess Again")
+
+// }
+// else{
+// alert('You Guessed Wrong, Try Again')
+// }
+
+
+
+
+
+
+
+
+// var username= prompt(" Enter Your Name Please ", "Zara")
+// console.log("Huzaira")
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var WorldcupSquad = ['Babar', 'Rizwan', 'Shadab', 'Fakhar', 'Shoaib', 'Shaheen', 'Haris', 'Imad','Hasan','Asif','Faheem','Naseem','Amir','Imam','Husnain']
+
+
+// var Playing11AgainstIndia= WorldcupSquad.slice(0,11)
+// console.log(WorldcupSquad)
+// console.log(Playing11AgainstIndia)
+
+
+
+
+
+
+
+
+
+
+// var nationality= prompt("Enter Your Nationality")
+
+
+// if (nationality =='Pakistani'){
+//     var gender= prompt("Enter Your Gender");
+//     if (gender=='Male','Female' ){  var age= prompt("Enter Your Age")  } if(age >=18 ) {alert("You are eligiable to vote")} if(age <=18)
+//     {alert("You are not eligiable to vote")}}
+  
+//  if(nationality=='Indian')
+ 
+//   {var gender= prompt("Enter Your Gender");
+//   if (gender=='Male','Female' ){  var age= prompt("Enter Your Age")  } if(age >=18 ) {alert("You are eligiable to vote")}if(age <=18)
+//   {alert("You are not eligiable to vote")}}
+
+//  else{ alert("You are not eligiable to vote")}
+
+
+
+
+
+
+
+
+
+// var ContactNumber = []
+// var ContactNames = []
+// var ContactNumber= prompt("Enter your Contact Number")
+// var ContactNames= prompt("Enter your ContactNames")
+
+// console.log(ContactNumber)
+// console.log(ContactNames)
+
+
+
+
+
+
